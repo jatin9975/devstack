9837e90cf830f4d2cbb3d3c85f34c052a0895840
jatin9975 <jatinmistry9975@gmail.com>
2024-05-24 14:56:51 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,11 +3,11 @@
 import mysql from 'mysql';
 
 const db = mysql.createConnection({
-  host: '127.0.0.1', // Use IPv4 address
+  host: process.env.DB_HOST, // Use IPv4 address
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: 3306 // Default MySQL port
+
 });
 
 db.connect((err) => {
