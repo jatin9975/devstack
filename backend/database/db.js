02b4eb640c4194807b9c18024d2f0bacd311a116
jatin9975 <jatinmistry9975@gmail.com>
2024-05-24 14:21:11 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,10 +3,10 @@
 import mysql from 'mysql';
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'devstack'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 db.connect((err) => {
